f6f5b427af3bbdc385d7a76e14116aa81f37a967
mohammadassia993-jpg <mohammadassia993@gmail.com>
2026-09-25 08:45:18 +0300

    feat(server): add /wallets.html handler

--- src/server.js
+++ src/server.js
@@ -314,6 +314,13 @@ export async function startServer() {
   const response = new Response(html, { headers: { 'content-type': 'text/html; charset=utf-8' }});
   return response.end(html);
 }
+if (url.pathname === '/wallets.html') {
+  const html = await fs.readFile(path.join(__dirname, 'public', 'wallets.html'), 'utf8');
+  const response = new Response(html, {
+    headers: { 'content-type': 'text/html; charset=utf-8' }
+  });
+  return response;
+}
 if (url.pathname === '/dashboard.js' && request.method === 'GET') {
         try {
           const jsContent = await fs.readFile(path.join(config.root, 'public', 'dashboard.js'), 'utf8');
